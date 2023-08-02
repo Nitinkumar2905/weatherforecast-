c486bb94e661e2ce26b736f2aeef657037c35e65
Nitin kumar <nimble2905@gmail.com>
2023-08-02 12:41:48 +0530

    some changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./img/icons8-forecast-30.png";
+import './Navbar.css'
+
 const Navbar = () => {
+  const [liveLocation, setLiveLocation] = useState("");
+  const [weatherData, setWeatherData] = useState([""]);
+  const [loading, setLoading] = useState(true);
+  // function to get live location
+  const getGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLiveLocation(
+            `${position.coords.latitude},${position.coords.longitude}`
+          );
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error.message);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  };
+
+  const handleWeatherData = async () => {
+    const url = `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${liveLocation}&country=india&days=3`;
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "6df67ca40emsh54451cad843c528p17f8b9jsn4d9fa3d544e6",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      setWeatherData({
+        city: data.location.name,
+        temp: data.current.temp_c,
+        humidity: data.current.humidity,
+        wind_speed: data.current.wind_kph,
+        vis: data.current.vis_km,
+        pressure: data.current.pressure_mb,
+        condition: data.current.condition.text,
+        icon: data.current.condition.icon,
+        uv: data.current.uv,
+        wind_dir: data.current.wind_dir,
+        date: data.current.last_updated,
+      });
+      console.log(data.current);
+      console.log("navbar");
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // Get the user's geolocation when the component mounts
+    getGeolocation();
+  }, []);
+
+  useEffect(() => {
+    handleWeatherData();
+  }, [liveLocation]);
   return (
     <>
-      <nav className="d-flex justify-content-center" style={{width:'100vw',height:'15vh'}}>
-        <div className="align-items-center d-flex" style={{width:'80vw'}}>
-          <img src={logo} alt="" />
-          <span className="text-primary" style={{fontSize:'2rem',fontFamily:'sans-serif'}}>Weather Forecast</span>
+      <nav
+        className="d-flex justify-content-center"
+      >
+        <div
+        className="d-flex align-items-center justify-content-between"
+         style={{width:'50vw'}}
+        id="main-nav"
+        >
+          <div className="align-items-center d-flex">
+            <img src={logo} alt="" />
+            <span
+              className="text-primary"
+            id="logo-text"
+            >
+              Weather forecast
+            </span>
+          </div>
+          {/* weather current live location */}
+          {!loading && (
+            <div>
+              <div className="d-flex  align-items-center justify-content-between" id="navbar-weather">
+                <img style={{height:'40px', width:'40px'}} className="mx-2" src={weatherData.icon} alt="" />
+                <div>
+                  <span className="mx-2">{weatherData.city}</span>
+                  <span className="mx-2">{weatherData.temp}°C</span>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </nav>
     </>
--- src/Components/WeatherData.jsx
+++ src/Components/WeatherData.jsx
@@ -16,26 +16,21 @@ import loadingt from "./img/loadingt.gif";
 const WeatherData = () => {
   const [info, setInfo] = useState({
     city: "",
-    temp: "",
-    humidity: "",
-    wind_speed: "",
-    visibility: "",
-    pressure: "",
-    condition: "",
   });
   const [weatherForecastData, setWeatherForecastData] = useState([]);
   const [location, setLocation] = useState("");
   const [loading, setLoading] = useState(true);
-  //   const [searchLocation, setSearchLocation] = useState("");
+  const [locationName, setLocationName] = useState("");
 
   // Function to get the user's current location
   const getGeolocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setLocation(
-            `${position.coords.latitude},${position.coords.longitude}`
-          );
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          setLocation(`${latitude},${longitude}`);
+          reverseGeocode(latitude, longitude); // Call reverse geocoding
         },
         (error) => {
           console.error("Error getting geolocation:", error.message);
@@ -43,6 +38,22 @@ const WeatherData = () => {
       );
     } else {
       console.log("Geolocation is not supported by this browser.");
+    }
+  };
+
+  // Function to perform reverse geocoding
+  const reverseGeocode = async (lat, lng) => {
+    const apiKey = "f44286e5b232438aa5542d6c5d2b26e6"; // Replace with your API key
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${apiKey}`;
+
+    try {
+      const response = await fetch(url);
+      if (response.data.results.length > 0) {
+        setLocationName(response.data.results[0].formatted_address);
+        console.log(locationName)
+      }
+    } catch (error) {
+      console.error("Reverse geocoding error:", error);
     }
   };
 
@@ -64,20 +75,9 @@ const WeatherData = () => {
       const response = await fetch(url, options);
       const data = await response.json();
       setInfo({
-        city: data.location.name,
-        temp: data.current.temp_c,
-        humidity: data.current.humidity,
-        wind_speed: data.current.wind_kph,
-        vis: data.current.vis_km,
-        pressure: data.current.pressure_mb,
-        condition: data.current.condition.text,
-        icon: data.current.condition.icon,
-        uv: data.current.uv,
-        wind_dir: data.current.wind_dir,
-        date: data.current.last_updated,
-      });
+        city:data.location.name
+      })
       setWeatherForecastData(data.forecast.forecastday);
-      console.log(data);
       console.log(data.forecast);
       setLoading(false);
     } catch (error) {
@@ -97,95 +97,25 @@ const WeatherData = () => {
   return (
     <>
       {!loading ? (
-        <div className="mt-3 d-flex flex-column align-items-center justify-content-center">
-          {/* <div className="mb-5 d-flex position-relative rounded-2 px-3 py-2" style={{color:'yellow',backgroundColor:'black'}}>
-            Note :&nbsp; &nbsp;
-            <div className="d-flex flex-column" style={{color:'white',width:'30vw'}}>
-              <span>
-                1. You can use coordinates for particular area's weather report.
-              </span>
-              <span style={{display:'flex',flexWrap:'wrap'}}>
-                2. Weather report for your location may not be 100% correct because of security concerns and use of VPNs.
-              </span>
-            </div>
-          </div> */}
-          <div className="d-flex">
+        <div className="d-flex flex-column align-items-center justify-content-center" style={{margin:'1rem 0rem' ,maxWidth:'100vw'}}>
+          <div className="d-flex" id="search-bar">
             <h5 className="mx-2 my-2">Search location</h5>
             <input
               onChange={handleLocationChange}
               className="border-black px-2 py-0 rounded-2 border-2 mx-2"
               type="text"
               placeholder="location"
-              value={location}
+                value={location}
             />
-            {/* <button
-              onClick={weatherData}
-              className="btn btn-primary mx-2 shadow-sm"
-            >
-              Check Weather
-            </button> */}
           </div>
 
           <div className="d-flex mt-2" id="weather-output">
             {/* output */}
-            <div className="mt-5">
-              <h5 className="mx-4 mb-2 text-center ">Today's weather</h5>
-              <div
-                className="m-3 p-4 rounded-4 d-flex flex-column mx-auto bg-info text-dark"
-                style={{
-                  width: "fit-content",
-                  border: "2px solid black",
-                }}
-              >
-                {info.city && (
-                  <span className="fw-bold mb-1">
-                    Weather in {info.city}: {info.temp}°C
-                  </span>
-                )}
-                <span className="mb-2">
-                  <span className="fw-bold">Date:</span> {info.date}
-                </span>
-                {info.icon ===
-                "//cdn.weatherapi.com/weather/64x64/day/113.png" ? (
-                  <WiDaySunny size={32} />
-                ) : (
-                  <WiDayFog size={32} />
-                )}
-                <span className="mb-1">
-                  <WiHumidity /> <span className="fw-bold"> Humidity:</span>{" "}
-                  {info.humidity}%
-                </span>
-                <span className="mb-1">
-                  <WiStrongWind /> <span className="fw-bold"> Wind Speed:</span>{" "}
-                  {info.wind_speed} kph
-                </span>
-                <span className="mb-1">
-                  <WiDayFog /> <span className="fw-bold"> Visibility:</span>{" "}
-                  {info.vis} km
-                </span>
-                <span className="mb-1">
-                  <WiBarometer /> <span className="fw-bold"> Pressure:</span>{" "}
-                  {info.pressure} mb
-                </span>
-                <span className="mb-1">
-                  <WiCloud /> <span className="fw-bold"> Condition:</span>{" "}
-                  {info.condition}
-                </span>
-                <span className="mb-1">
-                  {" "}
-                  <span className="fw-bold"> Uv Rays risk :</span> {info.uv}/10
-                </span>
-                <span className="mb-1">
-                  <WiStrongWind />{" "}
-                  <span className="fw-bold">Wind direction:</span>
-                  {info.wind_dir}
-                </span>
-              </div>
-            </div>
 
             {/* Weather forecast for three days */}
             {weatherForecastData.length > 0 && (
-              <div className="mt-5 mx-4 d-flex flex-column">
+              <div className="d-flex flex-column"
+              style={{margin:'2rem 0rem'}}>
                 <h5 className="text-center">
                   Weather forecast for next 3 days
                 </h5>
@@ -201,6 +131,7 @@ const WeatherData = () => {
                             border: "2px solid black",
                           }}
                         >
+                            <h5 className="mb-2 text-dark fw-bold">Location : {info.city}</h5>
                           <span>
                             <span className="fw-bold">Date:</span>{" "}
                             {forecast.date}
